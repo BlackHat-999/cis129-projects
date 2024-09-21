1fc84847319dc86c64d218b077b954f15d7d2a7b
BlackHat-999 <abegay18@mail.pima.edu>
2024-09-20 22:42:05 -0700

    Update cis129_lab02_shortStory.py
    Added authors name, lab description and code description.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Alec Begay
+# My first Python lab in CIS129 course
+# This code is an implenmentation of the fibonacci sequence in Python
+a, b = 0, 1
+print("Initial values: a =", a, "b =", b)
+while a < 100:
+    print("Inside loop: a =", a)
+    print(a); a, b = b, a + b
+    print("After iteration: a =", a, "b =", b)
